49d17866768e80547bedf6b1da4bbdd77ac510d6
falaiwi <fatimaalaiwi99@hotmail.com>
2026-01-11 00:07:58 +0300

    showing current level

--- components/renderLevel.js
+++ components/renderLevel.js
@@ -2,27 +2,26 @@ import { graphqlRequest } from "../services/graphql.js";
 
 export async function renderLevel(container) {
   const query = `
-  {
-  progress(
-    where: { grade: { _is_null: false } }
-    order_by: { grade: desc }
-    limit: 1
-  ) {
-    grade
-    path
-    createdAt
-  }
-}
-
+    {
+      progress(
+        where: { grade: { _is_null: false } }
+        order_by: { grade: desc }
+        limit: 1
+      ) {
+        grade
+      }
+    }
   `;
 
   const data = await graphqlRequest(query);
-
   const current = data.progress[0];
-  const level = current?.grade ?? 0;
+
+  const raw = current?.grade ?? 0;
+  const level = Math.round(raw * 10);
 
   container.innerHTML += `
     <h3>Current Level</h3>
     <p><strong>Level ${level}</strong></p>
   `;
 }
+
